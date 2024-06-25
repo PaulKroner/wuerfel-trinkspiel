14b91b341cb646df86e7caa964f3cbc101a20cb6
Paul K <paulfriedrich.kroener@gmail.com>
2024-06-25 23:54:30 +0200

    added home page

---
+++ src/app/routes.ts
@@ -0,0 +1,12 @@
+import {Routes} from '@angular/router';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+
+const routeConfig: Routes = [
+    {
+        path: "",
+        component: LandingPageComponent,
+        title: "Home page",
+    }
+];
+
+export default routeConfig;
