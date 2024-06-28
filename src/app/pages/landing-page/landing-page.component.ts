6e8380109d4b52a62b47ad945ffb5885a1ec7072
Paul Kröner <paulfriedrich.kroener@gmail.com>
2024-06-28 17:18:45 +0200

    added some tailwind styles

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AccordionComponent } from '../../UI-components/accordion/accordion.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [AccordionComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
