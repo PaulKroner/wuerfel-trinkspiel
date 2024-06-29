0f1dddfaeee7567d777fd00d3d3c2fec781ec3a9
Paul Kröner <paulfriedrich.kroener@gmail.com>
2024-06-29 17:14:38 +0200

    changed landing page style

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AccordionComponent } from '../../UI-components/accordion/accordion.component';
+import { DicePresetComponent } from '../../UI-components/dice-preset/dice-preset.component';
+import { DicePresetInterface } from '../../services/interfaces/dicepreset';
+import { DicePresetService } from '../../services/dice-preset-service.service';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [AccordionComponent],
+  imports: [CommonModule, AccordionComponent, DicePresetComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
+  presetList: DicePresetInterface[] = [];
+
+  constructor(private dicePresetService: DicePresetService) {}
+
+  ngOnInit(): void {
+    this.presetList = this.dicePresetService.getAllPresets();
+  }
 
 }
