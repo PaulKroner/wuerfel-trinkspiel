12cae080d6473bc78c634deb477862682f761e27
Paul Kröner <paulfriedrich.kroener@gmail.com>
2024-07-05 11:30:54 +0200

    tried routing but does not work

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -6,22 +6,27 @@ import { DicePresetInterface } from '../../services/interfaces/dicepreset';
 import { DicePresetService } from '../../services/dice-preset-service.service';
 import { TooltipComponent } from '../../UI-components/tooltip/tooltip.component';
 import { ModalComponent } from '../../UI-components/modal/modal.component';
+import { Router } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, AccordionComponent, DicePresetComponent, TooltipComponent, ModalComponent],
+  imports: [CommonModule, AccordionComponent, DicePresetComponent, TooltipComponent, ModalComponent, RouterOutlet],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
   presetList: DicePresetInterface[] = [];
   text = "Wie wird gespielt?";
 
-  constructor(private dicePresetService: DicePresetService) {}
+  constructor(private dicePresetService: DicePresetService, private router: Router) {}
 
   ngOnInit(): void {
     this.presetList = this.dicePresetService.getAllPresets();
   }
 
+  navigateToGame(): void {
+    this.router.navigate(['/gamescreen']);
+  }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,17 @@
-import {Routes} from '@angular/router';
-import {LandingPageComponent} from './pages/landing-page/landing-page.component';
+import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { GameScreenComponent } from './pages/game-screen/game-screen.component';
 
 const routeConfig: Routes = [
     {
         path: "",
         component: LandingPageComponent,
         title: "Home page",
+    },
+    {
+        path: "gamescreen",
+        component: GameScreenComponent,
+        title: "GameScreen",
     }
 ];
 
